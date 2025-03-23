30bccdf7fe85dc1e7547fe09f046f0dd84150b7b
tlk9919 <tlk_sure@126.com>
2025-03-23 18:13:58 +0800

    支付服务拆分，并修复openfeign-client客户端抽象方法上带注解

--- hmall/hm-api/src/main/java/com/hmall/api/client/CartClient.java
+++ hmall/hm-api/src/main/java/com/hmall/api/client/CartClient.java
@@ -2,11 +2,12 @@ package com.hmall.api.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Collection;
 
 @FeignClient("cart-service")
 public interface CartClient {
-    @DeleteMapping
-    void removeByItemIds(Collection<Long> itemIds);
+    @DeleteMapping("/carts")
+    void removeByItemIds(@RequestParam("ids")  Collection<Long> itemIds);
 }
