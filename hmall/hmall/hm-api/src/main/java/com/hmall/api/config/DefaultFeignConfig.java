dbc4b4a96da16f77d5f169dd259cab176c77ed8e
tlk9919 <tlk_sure@126.com>
2025-03-24 18:54:41 +0800

    网关-传递用户信息到微服务-openFeign传递用户信息

--- hmall/hmall/hm-api/src/main/java/com/hmall/api/config/DefaultFeignConfig.java
+++ hmall/hmall/hm-api/src/main/java/com/hmall/api/config/DefaultFeignConfig.java
@@ -1,11 +1,26 @@
 package com.hmall.api.config;
 
+import com.hmall.common.utils.UserContext;
 import feign.Logger;
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 public class DefaultFeignConfig {
     @Bean
     public Logger.Level feignLoggerLevel() {
         return Logger.Level.FULL;
+    }
+    @Bean
+    public RequestInterceptor requestUserInterceptor() {
+        return new RequestInterceptor() {
+            @Override
+            public void apply(RequestTemplate template) {
+                Long userId = UserContext.getUser();
+                if (userId !=null)
+                template.header("user-info",userId.toString());
+            }
+        };
     }
 }
