0ab73ccf32b8b81fd8d2608b5fa84d8b934c7d25
tlk9919 <tlk_sure@126.com>
2025-03-24 18:00:22 +0800

    网关-传递用户信息到微服务-mvc排除网关

--- hmall/hmall/hm-common/src/main/java/com/hmall/common/config/MvcConfig.java
+++ hmall/hmall/hm-common/src/main/java/com/hmall/common/config/MvcConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
-
+@ConditionalOnClass(DispatcherServlet.class)
 public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
