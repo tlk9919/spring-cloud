d6049ceacdc47c54c2f7fe98f947861f0f908499
tlk9919 <tlk_sure@126.com>
2025-03-26 10:55:32 +0800

    配置管理-动态路由

---
+++ hmall/hmall/hm-gateway/src/main/java/com/hmall/gateway/routes/DynamicRouteLoader.java
@@ -0,0 +1,57 @@
+package com.hmall.gateway.routes;
+
+import cn.hutool.json.JSONUtil;
+import com.alibaba.cloud.nacos.NacosConfigManager;
+import com.alibaba.nacos.api.config.listener.Listener;
+import com.alibaba.nacos.api.exception.NacosException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.route.RouteDefinition;
+import org.springframework.cloud.gateway.route.RouteDefinitionWriter;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+import java.util.concurrent.Executor;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class DynamicRouteLoader {
+    private final NacosConfigManager nacosConfigManager;
+    private final RouteDefinitionWriter routeDefinitionWriter;
+    private final String dataId = "hmall-gateway-routes";
+    private final String group = "DEFAULT_GROUP";
+    @PostConstruct
+    public void initRouteConfigListener() throws NacosException {
+        //1.项目启动时，先拉取一下配置，并监听配置的更新
+        String configInfo = nacosConfigManager.getConfigService()
+                .getConfigAndSignListener(dataId, group, 5000, new Listener() {
+                    @Override
+                    public Executor getExecutor() {
+                        return null;
+                    }
+
+                    @Override
+                    public void receiveConfigInfo(String configInfo) {
+                        //2. 监听到配置更新，更新路由表
+                        updateConfigInfo(configInfo);
+                    }
+
+
+                });
+        //3. 第一次读取到配置，也需要加载到路由表
+        updateConfigInfo(configInfo);
+    }
+    private void updateConfigInfo(String configInfo) {
+        //1.解析配置信息，转为RouteDefinition对象
+        List<RouteDefinition> routeDefinitions = JSONUtil.toList(configInfo, RouteDefinition.class);
+        //2。更新路由表
+        for (RouteDefinition routeDefinition : routeDefinitions) {
+            log.info("更新路由表：{}", routeDefinition);
+            //更新路由表
+            routeDefinitionWriter.save(Mono.just(routeDefinition)).subscribe();
+        }
+    }
+}
