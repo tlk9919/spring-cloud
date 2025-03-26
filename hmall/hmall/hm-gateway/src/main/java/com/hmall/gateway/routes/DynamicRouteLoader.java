2dc10bc287856655592434903bb0b42f60ec9ba4
tlk9919 <tlk_sure@126.com>
2025-03-26 11:13:11 +0800

    配置管理-动态路由-优化结构-更新时删除全部配置

--- hmall/hmall/hm-gateway/src/main/java/com/hmall/gateway/routes/DynamicRouteLoader.java
+++ hmall/hmall/hm-gateway/src/main/java/com/hmall/gateway/routes/DynamicRouteLoader.java
@@ -12,7 +12,9 @@ import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
 import javax.annotation.PostConstruct;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.Executor;
 
 @Component
@@ -21,8 +23,9 @@ import java.util.concurrent.Executor;
 public class DynamicRouteLoader {
     private final NacosConfigManager nacosConfigManager;
     private final RouteDefinitionWriter routeDefinitionWriter;
-    private final String dataId = "hmall-gateway-routes";
+    private final String dataId = "hmall-gateway-routes.json";
     private final String group = "DEFAULT_GROUP";
+    private final Set<String> routeIds=new LinkedHashSet<>();
     @PostConstruct
     public void initRouteConfigListener() throws NacosException {
         //1.项目启动时，先拉取一下配置，并监听配置的更新
@@ -45,13 +48,22 @@ public class DynamicRouteLoader {
         updateConfigInfo(configInfo);
     }
     private void updateConfigInfo(String configInfo) {
+        log.debug("监听到配置更新，更新路由表：{}", configInfo);
         //1.解析配置信息，转为RouteDefinition对象
         List<RouteDefinition> routeDefinitions = JSONUtil.toList(configInfo, RouteDefinition.class);
-        //2。更新路由表
+        //2.删除旧的路由表
+        routeIds.forEach(routeId -> {
+            routeDefinitionWriter.delete(Mono.just(routeId)).subscribe();
+        });
+        routeIds.clear();
+
+        //3.更新路由表
         for (RouteDefinition routeDefinition : routeDefinitions) {
             log.info("更新路由表：{}", routeDefinition);
-            //更新路由表
+            //3.1更新路由表
             routeDefinitionWriter.save(Mono.just(routeDefinition)).subscribe();
+            //3.2记录旧的路由id，便于下一次直接删除
+            routeIds.add(routeDefinition.getId());
         }
     }
 }
