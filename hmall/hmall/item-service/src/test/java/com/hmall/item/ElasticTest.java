baac8d8bd4796a5115ba9e244e1f2d95e4603193
tlk9919 <tlk_sure@126.com>
2025-03-31 17:44:06 +0800

    DeleteIndexRequest

--- hmall/hmall/item-service/src/test/java/com/hmall/item/ElasticTest.java
+++ hmall/hmall/item-service/src/test/java/com/hmall/item/ElasticTest.java
@@ -1,9 +1,16 @@
 package com.hmall.item;
 
+import net.sf.jsqlparser.statement.create.index.CreateIndex;
 import org.apache.http.HttpHost;
 import org.aspectj.lang.annotation.Before;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.indices.CreateIndexRequest;
+import org.elasticsearch.client.indices.GetIndexRequest;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -33,8 +40,73 @@ public class ElasticTest {
         System.out.println("连接成功client"+client);
     }
 
+    @Test
+    void testCreateIndex() throws IOException {
+        //1.准备Request对象
+        CreateIndexRequest request = new CreateIndexRequest("items");
+        //2.准备请求参数
+        request.source(MAPPING_TEMPLATE, XContentType.JSON);
+
+        //3.发送请求
+        client.indices().create(request, RequestOptions.DEFAULT);
+    }
+    @Test
+    void testGetIndex() throws IOException {
+        //1.准备Request对象
+        GetIndexRequest request = new GetIndexRequest("items");
+        //2.发送请求
+        boolean exists = client.indices().exists(request, RequestOptions.DEFAULT);
+        System.out.println("exists = " + exists);
+    }
+
+    @Test
+    void testDeleteIndex() throws IOException {
+        //1.准备Request对象
+        DeleteIndexRequest request = new DeleteIndexRequest("items");
+        //2.发送请求
+       client.indices().delete(request, RequestOptions.DEFAULT);
+
+    }
+
+
     @AfterAll
     static void afterAll() {
 
     }
+    private final static String MAPPING_TEMPLATE = "{\n" +
+            "  \"mappings\": {\n" +
+            "    \"properties\": {\n" +
+            "      \"id\": {\n" +
+            "        \"type\": \"keyword\"\n" +
+            "      },\n" +
+            "      \"name\": {\n" +
+            "        \"type\": \"text\",\n" +
+            "        \"analyzer\": \"ik_smart\"\n" +
+            "      },\n" +
+            "      \"price\": {\n" +
+            "        \"type\": \"integer\"\n" +
+            "      },\n" +
+            "      \"image\": {\n" +
+            "        \"type\": \"keyword\",\n" +
+            "        \"index\": false\n" +
+            "      },\n" +
+            "      \"category\": {\n" +
+            "        \"type\": \"keyword\"\n" +
+            "      },\n" +
+            "      \"brand\": {\n" +
+            "        \"type\": \"keyword\"\n" +
+            "      },\n" +
+            "      \"sold\": {\n" +
+            "        \"type\": \"integer\"\n" +
+            "      },\n" +
+            "      \"commentCount\": {\n" +
+            "        \"type\": \"integer\",\n" +
+            "        \"index\": false\n" +
+            "      },\n" +
+            "      \"isAd\": {\n" +
+            "        \"type\": \"boolean\"\n" +
+            "      }\n" +
+            "    }\n" +
+            "  }\n" +
+            "}\n";
 }
